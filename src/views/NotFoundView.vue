dde8bcec9ce6eca8c34e95e9bd1f3df3cdc18caf
Anastasia-Gorlina <115715396+Anastasia-Gorlina@users.noreply.github.com>
2025-07-25 16:10:52 +0300

    TASK-4.2 - add  page found

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
     <div class="not-found">
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
-      <router-link to="/">Go to Episodes</router-link>
+      <router-link to="/RickAndMorty">Go to Episodes</router-link>
     </div>
   </template>
   
